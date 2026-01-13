7a9fcff392363bde6d3cc79f6d1dd39e5c930ba0
unknown <kovacs.mate.22@ady-nagyatad.lan>
2026-01-13 15:13:31 +0100

    Zsakó László gyak

---
+++ Zsako_Gyak/focistat/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace focistat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] hazai = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            //int[] vendeg = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[] hazai = { 1, - 1, 3 };
+            int[] vendeg = { 2, 4, -1 };
+
+            
+
+
+
+        }
+    }
+}
