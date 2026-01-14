396a548d49afef787e8170bc1f66eb023ac66352
Matthew-F4 <kovacs.mate.22@F4-53>
2026-01-14 09:40:30 +0100

    kartyajatek

---
+++ Zsako_Gyak/kartyajatek/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace kartyajatek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> kartyaSzam = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> aliz = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> botond = Console.ReadLine().Split(' ').Select(int.Parse). ToList();
+            int min = int.MaxValue;
+            int index = 0;
+            for(int k = 0; k < kartyaSzam[0]; k++) { 
+                for(int i = 0; i < botond.Count; i++)
+                {
+                    if(min > Math.Abs(aliz[k] - botond[i]))
+                    {
+                        min = Math.Abs(aliz[k] - botond[i]);
+                        index = i;
+
+                    }
+                }
+                botond.RemoveAt(index);
+            }
+        }
+    }
+}
